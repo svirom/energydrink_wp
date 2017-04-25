958034074166450e54080f074737751f9187229a
svirom <svirom@yahoo.com>
2017-04-25 20:00:04 +0300

    404

--- wp-content/themes/energydrink/404.php
+++ wp-content/themes/energydrink/404.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
 	<div class="main blanc_page">
-		<p class="error-404">Такой страницы не существует. Попробуйте другой вариант</p>
+		<img src="<?php echo get_template_directory_uri(); ?>/img/popup_mtv.png" alt="Energydrink" class="logo_404">
+		<p class="error-404">Такой страницы больше не существует или никогда не существовало</p>
+		<p class="error-404">404</p>
+		<img src="<?php echo get_template_directory_uri(); ?>/img/img_bulls_404.png" alt="Energydrink" class="logo_bulls">
 	</div>
 <?php get_footer(); ?>
